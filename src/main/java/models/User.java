c0a0b25f3b18e9a1a83f2fb11ba1c962ce964516
Angelo2918 <angelosima@icloud.com>
2024-11-25 18:17:55 +0200

    init

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,7 +1,39 @@
 package models;
 
-public class User(String username,String password,String firstName,String lastName) {
-    public static void main(String... args){
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    }
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private LocalDate registrationDate;
+    private String email;
+    private String username;
+    private boolean active;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    @JsonManagedReference
+    private Address address;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Order> orders;
+
 }
